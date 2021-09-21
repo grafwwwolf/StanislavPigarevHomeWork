c533c2112d646ab1835bffa0c027d15ff16d245b
Stanislav <grafwwwolf@yandex.ru>
2021-09-21 20:04:12 +0300

    HomeWork4

--- src/main/java/HomeWorkApp.java
+++ src/main/java/HomeWorkApp.java
@@ -1,161 +1,153 @@
-import java.lang.reflect.Array;
-import java.util.Arrays;
+
+        import java.util.Arrays;
+        import java.util.Random;
+        import java.util.Scanner;
 
 public class HomeWorkApp {
 
+    private static char[][] map;
+    private static final char X_DOT = 'X';
+    private static final char O_DOT = 'O';
+    private static final char EMPTY_DOT = '•';
+    private static final int mapSize = 3;
+    private static final Scanner SC = new Scanner(System.in);
+
     public static void main(String[] args) {
 
-//       1.
-        int[] myArray = {1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0};
-        changeArray(myArray);
-//       2.
-        int[] secondArray = new int[100];
-        secondArray = completionArray(secondArray);
-//       3.
-        int[] thirdArray = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        changeArrayForTask3(thirdArray);
-//       4.
-        int[][] squareArray = new int[8][8];
-        completionArray(squareArray);
-        printSquareArray(squareArray);
-//       5.
-        int[] fifthArray = completionArray(3, -63);
-//       6. *
-        findMinAndMaxValuesOfArray(thirdArray);
-//       7. *
-        System.out.println(checkBalance(thirdArray));
-//       8. ***
-        moveArray(thirdArray, -2);
+        initMap();
+        printMap();
+
+        while (true) {
+            humanTurn();
+            printMap();
+            if (isWin(X_DOT)) {
+                System.out.println("YOU WIN");
+                break;
+            }
+            if (isDraw()) {
+                break;
+            }
+            computerTurn();
+            printMap();
+            if (isWin(O_DOT)) {
+                System.out.println("YOU LOSE");
+                break;
+            }
+            if (isDraw()) {
+                break;
+            }
+        }
+
     }
 
-    //    принт двумерного массива
-    public static void printSquareArray(int[][] squareArray) {
-        for (int i = 0; i < squareArray.length; i++) {
-            for (int j = 0; j < squareArray.length; j++) {
-                System.out.print(squareArray[i][j] + " ");
+    private static boolean isWin(char dot) {
+        if (map[0][0] == dot && map[0][1] == dot && map[0][2] == dot){
+            return true;
+        }
+        if (map[1][0] == dot && map[1][1] == dot && map[1][2] == dot){
+            return true;
+        }
+        if (map[2][0] == dot && map[2][1] == dot && map[2][2] == dot){
+            return true;
+        }
+
+        if (map[0][0] == dot && map[1][0] == dot && map[2][0] == dot){
+            return true;
+        }
+        if (map[0][1] == dot && map[1][1] == dot && map[2][1] == dot){
+            return true;
+        }
+        if (map[0][2] == dot && map[1][2] == dot && map[2][2] == dot){
+            return true;
+        }
+
+        if (map[0][0] == dot && map[1][1] == dot && map[2][2] == dot){
+            return true;
+        }
+        if (map[2][0] == dot && map[1][1] == dot && map[0][2] == dot){
+            return true;
+        }
+
+        return false;
+    }
+
+
+    private static void humanTurn() {
+        int xCoordinate;
+        int yCoordinate;
+        System.out.println("Введите координаты в формате \"x пробел y\"");
+        do {
+            xCoordinate = -1;
+            yCoordinate = -1;
+
+            if (SC.hasNextInt()) {
+                xCoordinate = SC.nextInt();
+            }
+            if (SC.hasNextInt()) {
+                yCoordinate = SC.nextInt();
+            }
+            SC.nextLine();
+        } while (!isValidHumanTurn(xCoordinate, yCoordinate));
+    }
+
+    private static void computerTurn() {
+        int xCoordinate;
+        int yCoordinate;
+        Random random = new Random();
+        do {
+            xCoordinate = random.nextInt(mapSize);
+            yCoordinate = random.nextInt(mapSize);
+        } while (!isValidComputerTurn(xCoordinate, yCoordinate));
+    }
+
+    private static boolean isValidHumanTurn(int xCoordinate, int yCoordinate) {
+        if (xCoordinate < 1 || yCoordinate < 1 ||
+                xCoordinate > mapSize || yCoordinate > mapSize) {
+            System.out.println("Вы ввели неправильные координаты. Введите координаты в формате \"x пробел y\"");
+            return false;
+        }
+
+        if (map[xCoordinate - 1][yCoordinate - 1] == EMPTY_DOT) {
+            map[xCoordinate - 1][yCoordinate - 1] = X_DOT;
+            return true;
+        }
+        System.out.println("Вы ввели неправильные координаты. Введите координаты в формате \"x пробел y\"");
+        return false;
+    }
+
+    private static boolean isValidComputerTurn(int xCoordinate, int yCoordinate) {
+        if (map[xCoordinate][yCoordinate] == EMPTY_DOT) {
+            map[xCoordinate][yCoordinate] = O_DOT;
+            return true;
+        }
+        return false;
+    }
+
+    private static void printMap() {
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
+                System.out.print(map[i][j] + " ");
             }
             System.out.println();
         }
     }
 
-    //    метод к заданию 1
-    public static void changeArray(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == 0) {
-                array[i] = 1;
-            } else {
-                array[i] = 0;
+    private static void initMap() {
+        map = new char[mapSize][mapSize];
+        for (int i = 0; i < mapSize; i++) {
+            Arrays.fill(map[i], EMPTY_DOT);
+        }
+    }
+
+    private static boolean isDraw() {
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
+                if (map[i][j] == EMPTY_DOT) {
+                    return false;
+                }
             }
         }
-    }
-
-    //    метод к заданию 2
-    public static int[] completionArray(int[] array) {
-        int[] localArray = new int[array.length];
-        for (int i = 0; i < localArray.length; i++) {
-            localArray[i] = i + 1;
-        }
-        return localArray;
-    }
-
-    //    метод к заданию 3
-    public static void changeArrayForTask3(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] < 6) {
-                array[i] *= 2;
-            }
-        }
-    }
-
-    //    метод к заданию 4
-    public static void completionArray(int[][] array) {
-        for (int i = 0; i < array.length; i++) {
-            array[i][i] = 1;
-            array[i][array.length - (i + 1)] = 1;
-        }
-    }
-
-    //    метод к заданию 5
-    public static int[] completionArray(int len, int initialValue) {
-        int[] localArray = new int[len];
-        for (int i = 0; i < localArray.length; i++) {
-            localArray[i] = initialValue;
-        }
-        return localArray;
-    }
-
-    //    метод к заданию 6
-    public static void findMinAndMaxValuesOfArray(int[] array) {
-        if (array.length == 0) {
-            System.out.println("Массив пустой");
-            return;
-        }
-
-        int minValue = array[0];
-        int maxValue = array[0];
-
-        for (int i = 1; i < array.length; i++) {
-            if (array[i] > maxValue) {
-                maxValue = array[i];
-            }
-            if (array[i] < minValue) {
-                minValue = array[i];
-            }
-        }
-        System.out.println("Минимальное значение массива " + " равно " + minValue);
-        System.out.println("Максимальное значение массива " + " равно " + maxValue);
-    }
-
-    //    метод к заданию 7
-    public static boolean checkBalance(int[] array) {
-        int a = array.length - 1;
-        int b = 1;
-        int leftSum = array[0];
-        int rightSum = array[a];
-
-        for (int i = 1; i < array.length - 1; i++) {
-            if (leftSum <= rightSum) {
-                leftSum += array[b++];
-            } else {
-                rightSum += array[--a];
-            }
-        }
-        return leftSum == rightSum;
-    }
-
-    //    метод к заданию 8
-    public static void moveArray(int[] array, int n) {
-        if (array.length == 0 || array.length == 1 || n == 0 || n % array.length == 0) {
-            return;
-        }
-        if (n > array.length) {
-            n %= array.length;
-        }
-        if (n > 0) {
-            for (; n > 0; n--) {
-                moveArrayPlus(array);
-            }
-        } else {
-            for (; n < 0; n++) {
-                moveArrayMinus(array);
-            }
-        }
-    }
-//    доп методы к заданию 8
-
-    private static void moveArrayPlus(int[] array) {
-        int variable = array[array.length - 1];
-        for (int i = array.length - 1; i > 0; i--) {
-            array[i] = array[i - 1];
-        }
-        array[0] = variable;
-    }
-
-    private static void moveArrayMinus(int[] array) {
-        int variable = array[0];
-        for (int i = 0; i < array.length - 1; i++) {
-            array[i] = array[i + 1];
-        }
-        array[array.length - 1] = variable;
+        System.out.println("DRAW");
+        return true;
     }
 }
